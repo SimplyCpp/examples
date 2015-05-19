96854466c9851b2b6b35ca4dc925e77d62636722
Thiago Massari Guedes <thiedri@gmail.com>
2015-05-18 22:38:48 -0300

    Mudancas no hello

--- hello.cpp
+++ hello.cpp
@@ -11,12 +11,27 @@ int main(int argc, char *argv[]) {
 	 * and efficient, which BCPL was. I want both
 	 * (Bjarne Stroustrup, 2015-02-05)
 	 */
-        
+
+	//If it was plain C:
+	printf("Hello from Fabio !\n");
+	printf("Hello from Thiago !\n");
+	printf("Fabio hopes you like the site !\n");
+	printf("Thiago hopes you like the site !\n");
+
+	//If it was real C++ (supposing you'd found a reason for
+	//writing professionally a hello world program).
+	std::cout << "Hello from Fabio !" << std::endl;
+	std::cout << "Hello from Thiago !" << std::endl;
+	std::cout << "Fabio hopes you like the site !" << std::endl;
+	std::cout << "Thiago hopes you like the site !" << std::endl;
+
+	//Now let's just put some fun in this code
+	//using new c++1x features
 	std::array<std::string, 2> authors { "Fabio", "Thiago" };
 	
 	std::for_each(std::begin(authors), std::end(authors), 
 		[](const std::string &author) { 
-			std::cout << "Hello from " << author << "  !\n" ; 
+			std::cout << "Hello from " << author << " !\n" ;
 		});
 		
 	for(const auto &author : authors) {

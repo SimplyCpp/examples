3fd2caa4c2a75ca48661a9588613f680d1bb63fe
Thiago Massari Guedes <thiedri@gmail.com>
2015-05-31 22:36:05 -0300

    sizes terminado

--- sizes.cpp
+++ sizes.cpp
@@ -2,15 +2,17 @@
 //April 2015
 //http://www.simplycpp.com/
 
+//g++ -O2 -std=c++14 -o sizes sizes.cpp && ./sizes
+
 #include "example_main.h"
 
 #include <iostream>
 #include <iomanip>
+#include <vector>
 #include <cstring>
 
 using namespace std;
 
-//alignment to 4 bytes
 struct Field {
 	unsigned int f1;  //int=4 bytes
 	unsigned char f2; //char=1 byte
@@ -23,21 +25,36 @@ struct FieldC {
 
 struct FieldS {
 	unsigned short f1; //short=2 bytes
-	unsigned int f2;   //int=4 bytes;
+	unsigned int f2;   //int=4 bytes
 };
 
 struct Field3 {
 	unsigned short f1; //short=2 bytes
-	unsigned char f2;  //char=4 bytes;
-	unsigned int f3;   //int=4 bytes;
+	unsigned char f2;  //char=1 byte
+	unsigned int f3;   //int=4 bytes
+};
+
+struct Field4 {
+	unsigned char f1; //char=1 byte
+	unsigned int f2;  //int=4 bytes
+	unsigned char f3; //char=1 byte
+};
+
+struct Field5 {
+	unsigned int f2;  //int=4 bytes
+	unsigned char f1; //char=1 byte
+	unsigned char f3; //char=1 byte
 };
 
 template<typename T>
-void dump2() {
+void dump2(initializer_list<string> header) {
 	T t;
+	vector<string> vs = header;
 	int f1 = ((long long)&t.f1 - (long long)&t);
 	int f2 = ((long long)&t.f2 - (long long)&t);
-	cout << std::setbase(10) << "f: " << f1 << "-" << sizeof(t.f1) << ", " << f2 << "-" << sizeof(t.f2) 
+	cout << setbase(10) 
+		<< vs[0] << ":" << f1 << "-" << sizeof(t.f1) << ", " 
+		<< vs[1] << ":" << f2 << "-" << sizeof(t.f2) 
 		<< " => Total bytes: " << sizeof(T) <<  endl;
 
 	const int size = sizeof(T);
@@ -45,39 +62,25 @@ void dump2() {
 	t.f1=0; t.f2=0;
 	uint8_t *c = (uint8_t *)&t;
 	for(int i=0; i < size; i++) {
-	  cout<< std::setbase(16) << (int)*c << " ";
+	  cout<< setbase(16) << (int)*c << " ";
 	  c++;
 	}
-	cout << std::setbase(10) << " Wasted bytes: " << (sizeof(T)-sizeof(t.f1)-sizeof(t.f2));
+	cout << setbase(10) << " Wasted bytes: " << (sizeof(T)-sizeof(t.f1)-sizeof(t.f2));
 	cout << endl << endl;
 }
 
 template<typename T>
-void dump2(int vezes) {
+void dump3(initializer_list<string> header) {
 	T t;
-	T tv[vezes];
-	int f1 = ((long long)&t.f1 - (long long)&t);
-	int f2 = ((long long)&t.f2 - (long long)&t);
-	cout << "f: " << f1 << ", " << f2 << " => Total bytes: " << sizeof(T) <<  endl;
+	vector<string> vs = header;
 
-	const int size = sizeof(T);
-	memset(&t, 0xFF, size); //fill w/ FF
-	t.f1=0; t.f2=0;
-	uint8_t *c = (uint8_t *)&t;
-	for(int i=0; i < size; i++) {
-	  cout<< std::setbase(16) << (int)*c << " ";
-	  c++;
-	}
-	cout << endl << endl;
-}
-
-template<typename T>
-void dump3() {
-	T t;
 	int f1 = ((long long)&t.f1 - (long long)&t);
 	int f2 = ((long long)&t.f2 - (long long)&t);
 	int f3 = ((long long)&t.f3 - (long long)&t);
-	cout << "f: " << f1 << "-" << sizeof(t.f1) << ", " << f2 << "-" << sizeof(t.f2) << ", " << f3 << "-" << sizeof(t.f3) 
+	cout << setbase(10) 
+		<< vs[0] << ":" << f1 << "-" << f1+sizeof(t.f1) << ", " 
+		<< vs[1] << ":" << f2 << "-" << f2+sizeof(t.f2) << ", " 
+		<< vs[2] << ":" << f3 << "-" << f3+sizeof(t.f3) 
 		<< " => Total bytes: " << sizeof(T) <<  endl;
 
 	const int size = sizeof(T);
@@ -85,25 +88,21 @@ void dump3() {
 	t.f1=0; t.f2=0; t.f3=0;
 	uint8_t *c = (uint8_t *)&t;
 	for(int i=0; i < size; i++) {
-	  cout<< std::setbase(16) << (int)*c << " ";
+	  cout<< setbase(16) << (int)*c << " ";
 	  c++;
 	}
-	cout << std::setbase(10) << " Wasted bytes: " << (sizeof(T)-sizeof(t.f1)-sizeof(t.f2)-sizeof(t.f3));
+	cout << setbase(10) << " Wasted bytes: " << (sizeof(T)-sizeof(t.f1)-sizeof(t.f2)-sizeof(t.f3));
 	cout << endl << endl;
 }
 
 
 EXAMPLE_MAIN(sizes) {
-/*
-"int, char"
-"char, int"
-"short, int"
-"short, char, int"
-*/
-	dump2<Field>();
-	dump2<FieldC>();
-	dump2<FieldS>();
-	dump2<FieldS>(2);
-	dump3<Field3>();
+
+	dump2<Field>( {"int", "char"} );
+	dump2<FieldC>( {"char", "int"} );
+	dump2<FieldS>( {"short", "int"} );
+	dump3<Field3>( {"short", "char", "int"} );
+	dump3<Field4>( {"char", "int", "char"} );
+	dump3<Field5>( {"int", "char", "char"} );
 
 }
